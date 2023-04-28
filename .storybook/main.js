6d8800ca25df6f055b59fef997e97ad63fb28cb5
kristinmc42 <mccollum.k.j@gmail.com>
2023-04-28 17:32:20 -0400

    fixed color of list-item.TASK_ARCHIVED for accessibility

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,15 @@ module.exports = {
   // stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
+  docs: {
+    autodocs: true
+  }
 };
